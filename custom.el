6e952eb308d57ea8103ab6edeaaaf98c5e3d3f72
echozhangxiaomin <xiaomin.zhang@pplingo.com>
2022-07-05 10:34:41 +0800

    doom init

---
+++ custom.el
@@ -0,0 +1,30 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("db5b906ccc66db25ccd23fc531a213a1afb500d717125d526d8ff67df768f2fc" "98fada4d13bcf1ff3a50fceb3ab1fea8619564bb01a8f744e5d22e8210bfff7b" "f0eb51d80f73b247eb03ab216f94e9f86177863fb7e48b44aacaddbfe3357cf1" "97db542a8a1731ef44b60bc97406c1eb7ed4528b0d7296997cbb53969df852d6" "d916b686ba9f23a46ee9620c967f6039ca4ea0e682c1b9219450acee80e10e40" "835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" "1f1b545575c81b967879a5dddc878783e6ebcca764e4916a270f9474215289e5" "6c531d6c3dbc344045af7829a3a20a09929e6c41d7a7278963f7d3215139f6a7" "1bddd01e6851f5c4336f7d16c56934513d41cc3d0233863760d1798e74809b4b" "e19ac4ef0f028f503b1ccafa7c337021834ce0d1a2bca03fcebc1ef635776bea" "1278c5f263cdb064b5c86ab7aa0a76552082cf0189acf6df17269219ba496053" "4699e3a86b1863bbc695236036158d175a81f0f3ea504e2b7c71f8f7025e19e3" "4a5aa2ccb3fa837f322276c060ea8a3d10181fecbd1b74cb97df8e191b214313" "8d7b028e7b7843ae00498f68fad28f3c6258eda0650fe7e17bfb017d51d0e2a2" "5784d048e5a985627520beb8a101561b502a191b52fa401139f4dd20acb07607" "da186cce19b5aed3f6a2316845583dbee76aea9255ea0da857d1c058ff003546" "6c98bc9f39e8f8fd6da5b9c74a624cbb3782b4be8abae8fd84cbc43053d7c175" "a9a67b318b7417adbedaab02f05fa679973e9718d9d26075c6235b1f0db703c8" "f91395598d4cb3e2ae6a2db8527ceb83fed79dbaf007f435de3e91e5bda485fb" "84b14a0a41bb2728568d40c545280dbe7d6891221e7fbe7c2b1c54a3f5959289" "d89e15a34261019eec9072575d8a924185c27d3da64899905f8548cbd9491a36" "3e200d49451ec4b8baa068c989e7fba2a97646091fd555eca0ee5a1386d56077" "4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3" "f5b6be56c9de9fd8bdd42e0c05fecb002dedb8f48a5f00e769370e4517dde0e8" "57a29645c35ae5ce1660d5987d3da5869b048477a7801ce7ab57bfb25ce12d3e" "e8df30cd7fb42e56a4efc585540a2e63b0c6eeb9f4dc053373e05d774332fc13" "1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" "d268b67e0935b9ebc427cad88ded41e875abfcc27abd409726a92e55459e0d01" "00445e6f15d31e9afaa23ed0d765850e9cd5e929be5e8e63b114a3346236c44c" "76ed126dd3c3b653601ec8447f28d8e71a59be07d010cd96c55794c3008df4d7" "613aedadd3b9e2554f39afe760708fc3285bf594f6447822dd29f947f0775d6c" "7a7b1d475b42c1a0b61f3b1d1225dd249ffa1abb1b7f726aec59ac7ca3bf4dae" "fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c" "7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" "234dbb732ef054b109a9e5ee5b499632c63cc24f7c2383a849815dacc1727cb6" "745d03d647c4b118f671c49214420639cb3af7152e81f132478ed1c649d4597d" "830877f4aab227556548dc0a28bf395d0abe0e3a0ab95455731c9ea5ab5fe4e1" default))
+ '(package-selected-packages
+   '(nano-theme go-translate lsp-javacomp eww-lnum helm-fuzzy intellij-theme lsp-intellij lsp-java fzf org-plus-contrib))
+ '(warning-suppress-log-types
+   '(((flycheck syntax-checker))
+     (change-major-mode-after-body-hook)
+     (change-major-mode-after-body-hook)
+     (change-major-mode-after-body-hook)
+     (hack-local-variables-hook)
+     (java-mode-local-vars-hook)))
+ '(warning-suppress-types
+   '(((flycheck syntax-checker))
+     (change-major-mode-after-body-hook)
+     (change-major-mode-after-body-hook)
+     (change-major-mode-after-body-hook)
+     (hack-local-variables-hook)
+     (java-mode-local-vars-hook))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'customize-variable 'disabled nil)
